6491f5e7237d041ed6e69550e791e6329a9df2ae
Edson Angoti Júnior <angoti@gmail.com>
2022-05-23 21:28:54 -0300

    update

--- src/main/java/com/professorangoti/excel/repository/RoleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.professorangoti.excel.repository;
-
-import com.professorangoti.excel.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<User, Integer>{
-    
-}
